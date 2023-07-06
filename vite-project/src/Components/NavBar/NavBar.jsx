abb0c809fe763ddae86b7c5a4eb5a76fb192ba29
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-06 23:26:15 +0100

    Done with navbar styling

--- vite-project/src/Components/NavBar/NavBar.jsx
+++ vite-project/src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Nav from "../../Sass/NavBar.module.scss";
+import { BsCart4 } from "react-icons/bs";
+import HomeImg from "../../assets/meals.jpg";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <nav className={Nav.navbar}>
+        <h1>Ejiro's Kitchen</h1>
+        <div className={Nav["cart-section"]}>
+          <BsCart4></BsCart4>
+          <h4>Your Cart</h4>
+          <div className={Nav.count}>
+            <p>0</p>
+          </div>
+        </div>
+      </nav>
+      <div className={Nav.image}>
+        <img className={Nav["Curved-img"]} src={HomeImg} alt="" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default NavBar;

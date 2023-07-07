129e5f2fc2933dd2dc2b31a879a47e7f14c2b3d4
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-07 22:24:37 +0100

    MainHeader scetion doone

--- vite-project/src/Components/MainSection/MainHeader.jsx
+++ vite-project/src/Components/MainSection/MainHeader.jsx
@@ -10,6 +10,7 @@ const MainHeader = () => {
         Name: "Jollof Rice and Chicken",
         Ingredients: "Rice and Chicken",
         Amount: 1200,
+        id: 1,
       },
     ],
     SecondArr: [
@@ -17,6 +18,7 @@ const MainHeader = () => {
         Name: "Eba and Egusi Soup",
         Ingredients: "Garri and White-Seed-Melon",
         Amount: 1500,
+        id: 2,
       },
     ],
     ThirdArr: [
@@ -24,13 +26,15 @@ const MainHeader = () => {
         Name: "Sushi",
         Ingredients: "Finest fish and vegies",
         Amount: 3000,
+        id: 3,
       },
     ],
     FourthArr: [
       {
         Name: "Berbecue Burger",
-        Ingredients: "American raw and healthy",
+        Ingredients: "American, raw and healthy",
         Amount: 1500,
+        id: 4,
       },
     ],
   };
@@ -42,9 +46,27 @@ const MainHeader = () => {
       <NavBar></NavBar>
       <BodyModal></BodyModal>
 
-      <div className="mealSection">
+      <div className={MainBody.mealSection}>
         {mealSectionArray.map((item) => {
-          console.log(item[0].Name);
+          return (
+            <div key={item[0].id} className={MainBody.MealSectionBody}>
+              <div className={MainBody.mealSectionText}>
+                <h2>{item[0].Name}</h2>
+                <h4>{item[0].Ingredients}</h4>
+                <h1>{item[0].Amount}</h1>
+              </div>
+              <div className={MainBody.mealSectionLogic}>
+                <div className={MainBody.mealSectionInput}>
+                  <h2>Amount</h2>
+                  <input type="number" />
+                </div>
+                <button>+ Add</button>
+              </div>
+              <div className={MainBody.line}>
+                <hr />
+              </div>
+            </div>
+          );
         })}
       </div>
     </React.Fragment>

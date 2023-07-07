f7a8c87c9ab287951b57f93aa18d8227f509f050
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-07 10:51:14 +0100

    Mapped through meal values

--- vite-project/src/Components/MainSection/MainHeader.jsx
+++ vite-project/src/Components/MainSection/MainHeader.jsx
@@ -4,10 +4,49 @@ import BodyModal from "../Body/BodyModal";
 import MainBody from "../../Sass/MainHeader.module.scss";
 
 const MainHeader = () => {
+  const MealSection = {
+    FirstArr: [
+      {
+        Name: "Jollof Rice and Chicken",
+        Ingredients: "Rice and Chicken",
+        Amount: 1200,
+      },
+    ],
+    SecondArr: [
+      {
+        Name: "Eba and Egusi Soup",
+        Ingredients: "Garri and White-Seed-Melon",
+        Amount: 1500,
+      },
+    ],
+    ThirdArr: [
+      {
+        Name: "Sushi",
+        Ingredients: "Finest fish and vegies",
+        Amount: 3000,
+      },
+    ],
+    FourthArr: [
+      {
+        Name: "Berbecue Burger",
+        Ingredients: "American raw and healthy",
+        Amount: 1500,
+      },
+    ],
+  };
+
+  const mealSectionArray = Object.values(MealSection);
+
   return (
     <React.Fragment>
       <NavBar></NavBar>
       <BodyModal></BodyModal>
+
+      <div className="mealSection">
+        {mealSectionArray.map((item) => {
+          console.log(item[0].Name);
+        })}
+      </div>
     </React.Fragment>
   );
 };

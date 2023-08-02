37198c911fa5d360590ad411d528d778556b97da
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-02 01:29:00 +0100

    Done with img comonents

--- vite-project/src/pages/HomePage.jsx
+++ vite-project/src/pages/HomePage.jsx
@@ -1,16 +1,33 @@
 import { Fragment } from "react";
 import Classes from "../Sass/HomeNav.module.scss";
+import { Link } from "react-router-dom";
+import BgImg from "../assets/downloadEjiro.png";
+import Restaurant from "../assets/restaurant.jpg";
+import { Cursor } from "react-simple-typewriter";
 
 const HomeNavBar = () => {
   return (
     <Fragment>
       <nav className={Classes.navbar}>
-        <h1>Ejiro's Kitchen</h1>
-        <div className="login-page">
-          <li>Sign In</li>
-          <li>Sign up</li>
+        <div className={Classes.imgFlex}>
+          <img src={BgImg} alt="" />
+          <h1>Ejiro's Kitchen</h1>
+        </div>
+        <div className={Classes["login-page"]}>
+          <ul>
+            <li>
+              <Link>Sign In</Link>
+            </li>
+            <li>
+              <Link>Sign Up</Link>
+            </li>
+          </ul>
         </div>
       </nav>
+      <div className={Classes["Main-text-div"]}>
+        <img src={Restaurant} alt="" />
+        <div className="text-section"></div>
+      </div>
     </Fragment>
   );
 };
